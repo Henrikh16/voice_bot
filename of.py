2514879923c098979b57d6b6919b504ec3904ad4
Henrikh <68509136+Henrikh16@users.noreply.github.com>
2021-08-09 10:40:13 +0300

    Add files via upload

---
+++ of.py
@@ -0,0 +1,81 @@
+# Importing required modules
+import os
+import pyttsx3
+import speech_recognition as sr
+
+
+
+# Creating class
+class Gfg:
+
+    # Method to take choice commands as input
+    def takeCommand(self):
+
+          # Using Recognizer and Microphone Method for input voice commands
+          r = sr.Recognizer()
+          with sr.Microphone() as source:
+              print('Listening')
+
+              # Number pf seconds of non-speaking audio before
+              # a phrase is considered complete
+              r.pause_threshold = 0.7
+              audio = r.listen(source)
+
+              # Voice input is identified
+              try:
+
+                  # Listening voice commands in eng english
+                  print("Recognizing")
+                  Query = r.recognize_google(audio, language='en-GB')
+
+                  # Displaying the voice command
+                  print("the query is printed='", Query, "'")
+
+              except Exception as e:
+
+                  # Displaying exception
+                  print(e)
+                  # Handling exception
+                  print("Say that again sir")
+                  return "None"
+          return Query
+
+
+    # Method for voice output
+    def Speak(self, audio):
+
+          # Constructor call for pyttsx3.init()
+          engine = pyttsx3.init()
+
+          # Setting voice type and id
+          voices = engine.getProperty('voices')
+          engine.setProperty('voice', voices[1].id)
+          engine.say(audio)
+          engine.runAndWait()
+
+
+    # Method to self shut down system
+    def quitSelf(self):
+        self.Speak("Close all that shit?")
+
+        # Input voice command
+        take = self.takeCommand()
+        choice = take
+        if choice == 'yes':
+
+            # Shutting down
+            print("Shutting down the computer")
+            self.Speak("Goodbye, bitches, see you next start")
+            os.system("poweroff")
+        if choice == 'no':
+
+            # Idle
+            print("Thank u sir")
+            self.Speak("Fuck it, see you next time")
+
+
+
+# Driver code
+if __name__ == '__main__':
+    Maam = Gfg()
+    Maam.quitSelf()
